f71dc07dddcd0e1625411f3df10e97eac03ee50f
Lila Amelia <lilaamelia77@gmail.com>
2024-06-29 15:14:14 +0700

    display centered

--- rit-3/src/components/Display.jsx
+++ rit-3/src/components/Display.jsx
@@ -1,7 +1,7 @@
 const Display = () => {
   return (
     <div className="container-fluid">
-        <h3 className="fw-bold">Display</h3>
+        <h3 className="fw-bold text-center my-5">Display</h3>
     </div>
   )
 }
