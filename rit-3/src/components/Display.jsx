8c4f42bda74ff4e7eb689e478b4d3489eb04ac46
Lila Amelia <lilaamelia77@gmail.com>
2024-06-29 15:31:55 +0700

    test

--- rit-3/src/components/Display.jsx
+++ rit-3/src/components/Display.jsx
@@ -2,6 +2,7 @@ const Display = () => {
   return (
     <div className="container-fluid">
         <h3 className="fw-bold text-center my-5">Display</h3>
+        <p className="text-center">testing testing</p>
     </div>
   )
 }
